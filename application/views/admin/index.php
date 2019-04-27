4953fa0fea2774ded95cdb6e3a6678ddf2af7599
unknown <iinpradnya@gmail.com>
2019-04-28 03:04:10 +0800

    Tugas Praktikum Web-1608561014

---
+++ application/views/admin/index.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="en">
+    <?php $this->load->view('admin/layout/header2')?>
+    
+
+      <div id="content-wrapper">
+
+        <div class="container-fluid">
+
+          <!-- DataTables Example -->
+          <div class="card mb-3">
+            <div class="card-header">PROGRAM PERPUSTAKAAN</div>
+            <div class="card-body">
+              <div class="table-responsive">
+                <p>Nama : Putu Indah Pradnyawati</p>
+                <p>Nim  : 1608561014</p>
+                <p>Kelas : Praktikum Pemrograman Berbasis Web A</p> 
+              </div>
+            </div>
+          </div>
+
+        </div>
+        <!-- /.container-fluid -->
+        <?php $this->load->view('admin/layout/footer2') ?>
+  </body>
+
+</html>
